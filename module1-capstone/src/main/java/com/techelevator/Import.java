9a3e7e05002b333c18a2650a76d7798136b52592
Brandon Barnes <barnesbrandon@live.com>
2017-05-25 15:04:14 -0400

    WTF

--- module1-capstone/src/main/java/com/techelevator/Import.java
+++ module1-capstone/src/main/java/com/techelevator/Import.java
@@ -15,14 +15,17 @@ public class Import {
 	String[] documentArray;
 	
 	
-	public void readCSVFile() throws FileNotFoundException {
+	public void readCSVFile() {
 		String document = "";
 		String[] documentArray;
-		File f = new File("test.csv");
+		File f = new File("vendingmachine.csv");
 		try(Scanner s = new Scanner(f)) {
 		    while(s.hasNextLine()) {
 		        document += s.nextLine() + "\n";
 		    }
+		} catch (FileNotFoundException e) {
+			System.out.println("Was unable to locate file with name \"vendingmachine.csv\".");
+			e.printStackTrace();
 		}
 		documentArray = document.split("\n");
 		this.documentArray = documentArray;
@@ -33,7 +36,7 @@ public class Import {
 		
 		for(String element : documentArray) {
 			
-			String[] elementArray = element.split("|");
+			String[] elementArray = element.split("\\|");
 			String tempKey = elementArray[0];
 			List<Items> tempList = new ArrayList<>();
 			BigDecimal bd = new BigDecimal(elementArray[2]);
@@ -83,7 +86,9 @@ public class Import {
 
 	
 	//Goes through each specific item in the machine and sets the length of its list to 5 and then returns a map (aligning with its key).
-	public Map<String, List<Items>> stockMachine() {
+	public Map<String, List<Items>> stockMachine() throws FileNotFoundException {
+		readCSVFile();
+		createMap();
 		return vendingMap;
 	}
 }
--- module1-capstone/src/main/java/com/techelevator/VendingMachine.java
+++ module1-capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -46,7 +47,45 @@ public class VendingMachine {
 	
 	public void refill() {
 		
-		// will call importer and get a map
+		Import im = new Import();
+		try {
+			itemsInTheMachine = im.stockMachine();
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to locate file.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public void displayItems() {
+		
+		System.out.println();
+		
+		
+		// Look at all slots, then pull out the LIST of items in each slot
+		
+		if(itemsInTheMachine.isEmpty()) {
+			System.out.println("\nThe machine is empty.\n");
+			return;
+		}
+		
+		for (String key : itemsInTheMachine.keySet()) {
+			
+			List<Items> slotItems = itemsInTheMachine.get(key);
+			
+			// print out only available items
+			if(slotItems.size()>0)  {
+				
+				System.out.print(key + "\t");
+				System.out.print(slotItems.get(0).getItemName()+"\t");
+				System.out.print("(" + slotItems.size() + ")");
+				System.out.println();
+				
+			}
+			
+		}
+		
 		
 	}
 	
