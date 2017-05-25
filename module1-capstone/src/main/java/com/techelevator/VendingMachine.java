2489ac5d9bea25d795e52ef200624da86968834b
Brandon Barnes <barnesbrandon@live.com>
2017-05-25 15:02:47 -0400

    Updated Vending Machine

--- module1-capstone/src/main/java/com/techelevator/VendingMachine.java
+++ module1-capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -12,14 +12,17 @@ public class VendingMachine {
 	
 	public boolean isInStock(String key) {
 		if (itemsInTheMachine.get(key).size() > 0) {
+			System.out.println("The requested item is in stock.");
 			return true;
 		} else {
+			System.out.println("The requested item is not in stock.");
 			return false;
 		}
 	}
 
 	public boolean canPurchase(String key) {
 		if (itemsInTheMachine.get(key).get(0).getPrice().doubleValue() > availableFunds.doubleValue()){
+			System.out.println("You cannot purchase that item.");
 			return false;
 		}
 		
@@ -34,11 +37,10 @@ public class VendingMachine {
 	}
 	
 	
-	public BigDecimal feedMoney (BigDecimal amountInserted) {
-		
-		// will increase balance -- do we need validation?
-		
-		return null;
+	public void feedMoney (BigDecimal amountInserted) {
+		availableFunds = availableFunds.add(amountInserted);
+		System.out.println("Sucessfully inserted: $"+amountInserted.doubleValue());
+	
 	}
 	
 	
