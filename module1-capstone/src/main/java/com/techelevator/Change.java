255a06b7d2a3d00107a3e6104b056b223c1f8e53
Brandon Barnes <barnesbrandon@live.com>
2017-05-25 12:05:10 -0400

    Calculate Change

--- module1-capstone/src/main/java/com/techelevator/Change.java
+++ module1-capstone/src/main/java/com/techelevator/Change.java
@@ -5,10 +5,35 @@ import java.math.BigDecimal;
 public class Change {
 
 	public String calculateChange(BigDecimal amount) {
+		BigDecimal quarter = new BigDecimal(0.25);
+		BigDecimal dime = new BigDecimal(0.10);
+		BigDecimal nickle = new BigDecimal(0.05);
+		int numQuarters=0;
+		int numDimes=0;
+		int numNickles=0;
 		
 		
 		
-		return "";
+		while (amount.doubleValue() >0.00){
+		if (amount.doubleValue() > quarter.doubleValue()){
+			amount.subtract(quarter);
+			numQuarters = numQuarters +1;
+			
+			
+		} else if (amount.doubleValue() > dime.doubleValue()){
+			amount.subtract(dime);
+			numDimes = numDimes +1;
+			
+		} else {
+			amount.subtract(nickle);
+			numNickles = numNickles +1;
+			
+		}
+		}
+		
+		
+		BigDecimal totalChange = new BigDecimal(numQuarters * 0.25 + numDimes * 0.10 + numNickles * 0.05);
+		return "Your total change is: $" + totalChange.toString() + ": Quarters:" + numQuarters + ", Dimes:" + numDimes + ",  Nicles:" + numNickles;
 	}
 	
 
