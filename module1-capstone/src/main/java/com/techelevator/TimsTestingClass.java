a5545619d5588ce090a19d5f09130ff219521aff
Timothy Nevits <tim@timnevits.com>
2017-05-25 14:31:41 -0400

    in progress

--- module1-capstone/src/main/java/com/techelevator/TimsTestingClass.java
+++ module1-capstone/src/main/java/com/techelevator/TimsTestingClass.java
@@ -6,13 +6,21 @@ import java.math.BigDecimal;
 public class TimsTestingClass {
 	
 	public static void main(String[] args) throws IOException {
+		
+		VendingMachine vm = new VendingMachine();
+		
+		Import im = new Import();
+		
+		
+//	
+//	BigDecimal amount1 = new BigDecimal("10.00");
+//	BigDecimal amount2 = new BigDecimal("15.00");
 	
-	BigDecimal amount1 = new BigDecimal("10.00");
-	BigDecimal amount2 = new BigDecimal("15.00");
-	
-	Logger logger = new Logger();
-	
-	logger.log("PURCHASED SOCKS", amount1, amount2);
+	System.out.println(amount1.compareTo(amount2));
+
+//	Logger logger = new Logger();
+//	
+//	logger.log("PURCHASED SOCKS", amount1, amount2);
 	
 	}
 
--- module1-capstone/src/main/java/com/techelevator/VendingMachine.java
+++ module1-capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -40,7 +40,34 @@ public class VendingMachine {
 	
 	public void refill() {
 		
-		// will call importer and get a map
+		Import im = new Import();
+		itemsInTheMachine = im.stockMachine();
+		
+	}
+	
+	
+	public void displayItems() {
+		
+		System.out.println();
+		
+		
+		// Look at all slots, then pull out the LIST of items in each slot
+		
+		for (String key : itemsInTheMachine.keySet()) {
+			
+			List<Items> slotItems = itemsInTheMachine.get(key);
+			
+			// print out only available items
+			if(slotItems.size()>0)  {
+				
+				System.out.print(key + "\t");
+				System.out.print(slotItems.get(0).getItemName()+"\n");
+				System.out.print("(" + slotItems.size() + ")");
+				
+			}
+			
+		}
+		
 		
 	}
 	
