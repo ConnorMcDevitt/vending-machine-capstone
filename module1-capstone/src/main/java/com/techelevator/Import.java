dff6543a2ef18e2d944d3096c8315775e7391b2f
Connor McDevitt <connor1621@gmail.com>
2017-05-25 13:35:53 -0400

    Created Import class that read the input file and then creates a map of items based on the input file

--- module1-capstone/src/main/java/com/techelevator/Import.java
+++ module1-capstone/src/main/java/com/techelevator/Import.java
@@ -1,31 +1,88 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Import {
 	
-	List<Chip> listOfPotatoCrisps;
-	List<Chip> listOfStackers;
-	List<Chip> listOfGrainWaves;
-	List<Chip> listOfCloudPopcorn;
-	List<Candy> listOfMoonpie;
-	List<Candy> listOfCowtales;
-	List<Candy> listOfWonkaBar;
-	List<Candy> listOfCrunchie;
-	List<Drink> listOfCola;
-	List<Drink> listOfDrSalt;
-	List<Drink> listOfMountainMelter;
-	List<Drink> listOfHeavy;
-	List<Gum> listOfUChews;
-	List<Gum> listOfLittleLeagueChew;
-	List<Gum> listOfChiclets;
-	List<Gum> listOfTriplemint;
+	Map<String, List<Items>> vendingMap;
+	
+	
+	public String[] readCSVFile() throws FileNotFoundException {
+		String document = "";
+		String[] documentArray;
+		File f = new File("vendingmachine.csv");
+		try(Scanner s = new Scanner(f)) {
+		    while(s.hasNextLine()) {
+		        document += s.nextLine() + "\n";
+		    }
+		}
+		documentArray = document.split("\n");
+		return documentArray;
+	}
+	
+	public void createMap(String[] documentArray) {
+		Map<String, List<Items>> vendingMap = new HashMap<>();
+		
+		for(String element : documentArray) {
+			
+			String[] elementArray = element.split("|");
+			String tempKey = elementArray[0];
+			List<Items> tempList = new ArrayList<>();
+			BigDecimal bd = new BigDecimal(elementArray[2]);
+			
+			if(elementArray[0].startsWith("A")) {
+				
+				Chip chip = new Chip(elementArray[1], bd);
+				for(int i = 0; i < 5; i++) {
+					tempList.add(chip);
+				}
+				vendingMap.put(tempKey, tempList);
+				
+			} else if (elementArray[0].startsWith("B")) {
+				
+				Candy candy = new Candy(elementArray[1], bd);
+				for(int i = 0; i < 5; i++) {
+					tempList.add(candy);
+				}
+				vendingMap.put(tempKey, tempList);
+				
+			} else if (elementArray[0].startsWith("C")) {
+				
+				Drink drink = new Drink(elementArray[1], bd);
+				for(int i = 0; i < 5; i++) {
+					tempList.add(drink);
+				}
+				vendingMap.put(tempKey, tempList);
+				
+			} else if (elementArray[0].startsWith("D")) {
+				
+				Gum gum = new Gum(elementArray[1], bd);
+				for(int i = 0; i < 5; i++) {
+					tempList.add(gum);
+				}
+				vendingMap.put(tempKey, tempList);
+			}
+			
+			
+			
+			for(int i = 0; i < 5; i++) {
+			}
+			
+		}
+		this.vendingMap = vendingMap;
+	}
+	
+
 	
 	//Goes through each specific item in the machine and sets the length of its list to 5 and then returns a map (aligning with its key).
 	public Map<String, List<Items>> stockMachine() {
-		for(Items item : listOfItems);
-		
-		return null;
+		return vendingMap;
 	}
 }
