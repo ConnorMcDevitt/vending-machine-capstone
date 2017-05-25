56a7e54410458eabd7c142ea8830b2f6dd20479c
Timothy Nevits <tim@timnevits.com>
2017-05-25 11:14:37 -0400

    slowly filling in vending machine shell methods

--- module1-capstone/src/main/java/com/techelevator/VendingMachine.java
+++ module1-capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -1,5 +1,34 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
 public class VendingMachine {
+	
+	private Map<String,List<Items>> itemsInTheMachine;
+	private BigDecimal availableFunds;
+	
+	
+	public boolean isInStock(String key) {
+		
+		return false;
+	}
+	
+	
+	public void vend(String key) {
+		
+	}
+	
+	
+	public BigDecimal feedMoney (BigDecimal amountInserted) {
+		
+		return null;
+	}
+	
+	
+	
+	
+	
 
 }
