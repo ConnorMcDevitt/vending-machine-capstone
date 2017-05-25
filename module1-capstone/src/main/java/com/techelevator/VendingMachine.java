9d6bb801bb4c8aee73b898487ca916b1081593ec
Brandon Barnes <barnesbrandon@live.com>
2017-05-25 15:20:22 -0400

    Added Vend Method

--- module1-capstone/src/main/java/com/techelevator/VendingMachine.java
+++ module1-capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -6,11 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 public class VendingMachine {
-	
-	private Map<String,List<Items>> itemsInTheMachine;
+
+	private Map<String, List<Items>> itemsInTheMachine;
 	private BigDecimal availableFunds;
-	
-	
+
 	public boolean isInStock(String key) {
 		if (itemsInTheMachine.get(key).size() > 0) {
 			System.out.println("The requested item is in stock.");
@@ -22,31 +21,30 @@ public class VendingMachine {
 	}
 
 	public boolean canPurchase(String key) {
-		if (itemsInTheMachine.get(key).get(0).getPrice().doubleValue() > availableFunds.doubleValue()){
+		if (itemsInTheMachine.get(key).get(0).getPrice().doubleValue() > availableFunds.doubleValue()) {
 			System.out.println("You cannot purchase that item.");
 			return false;
 		}
-		
+
 		return true;
 	}
- 	
-	
+
 	public void vend(String key) {
+		availableFunds = availableFunds.subtract(itemsInTheMachine.get(key).get(0).getPrice());
+		itemsInTheMachine.get(key).remove(0);
 		
-	
+		
 		
 	}
-	
-	
-	public void feedMoney (BigDecimal amountInserted) {
+
+	public void feedMoney(BigDecimal amountInserted) {
 		availableFunds = availableFunds.add(amountInserted);
-		System.out.println("Sucessfully inserted: $"+amountInserted.doubleValue());
-	
+		System.out.println("Sucessfully inserted: $" + amountInserted.doubleValue());
+
 	}
-	
-	
+
 	public void refill() {
-		
+
 		Import im = new Import();
 		try {
 			itemsInTheMachine = im.stockMachine();
@@ -54,39 +52,35 @@ public class VendingMachine {
 			System.out.println("Unable to locate file.");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
+
 	public void displayItems() {
-		
+
 		System.out.println();
-		
-		
-	
-		
-		if(itemsInTheMachine.isEmpty()) {
+
+		if (itemsInTheMachine.isEmpty()) {
 			System.out.println("\nThe machine is empty.\n");
 			return;
 		}
-		
+
 		for (String key : itemsInTheMachine.keySet()) {
-			
+
 			List<Items> slotItems = itemsInTheMachine.get(key);
-			
+
 			// print out only available items
-			if(slotItems.size()>0)  {
-				
+			if (slotItems.size() > 0) {
+
 				System.out.print(key + "\t");
-				System.out.print(slotItems.get(0).getItemName()+"\t");
+				System.out.print(slotItems.get(0).getItemName() + "\t");
 				System.out.print("(" + slotItems.size() + ")");
 				System.out.println();
-				
-			}
-			
-		}}}
 
-	
+			}
+
+		}
+	}
+}
 	
 	
 		
