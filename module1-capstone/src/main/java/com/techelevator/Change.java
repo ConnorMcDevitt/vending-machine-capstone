ae82eb14e4d4aeab607917ca088522cccff3329d
Brandon Barnes <barnesbrandon@live.com>
2017-05-25 13:57:09 -0400

    Finished CalculateChange

--- module1-capstone/src/main/java/com/techelevator/Change.java
+++ module1-capstone/src/main/java/com/techelevator/Change.java
@@ -7,35 +7,41 @@ public class Change {
 	BigDecimal totalChange;
 	
 	
-	public String calculateChange(BigDecimal differencePrice) {
-		BigDecimal quarter = new BigDecimal(0.25);
-		BigDecimal dime = new BigDecimal(0.10);
-		BigDecimal nickle = new BigDecimal(0.05);
-		int numQuarters=0;
-		int numDimes=0;
-		int numNickles=0;
+	public String calculateChange(BigDecimal difference) {
+		
+		BigDecimal quarter = new BigDecimal("0.25");
+		BigDecimal dime = new BigDecimal("0.10");
+		BigDecimal nickle = new BigDecimal("0.05");
+		BigDecimal numQuarters = BigDecimal.ZERO;
+		BigDecimal numDimes = BigDecimal.ZERO;
+		BigDecimal numNickles = BigDecimal.ZERO;
 		
 		
 		
-		while (differencePrice.doubleValue() >0.00){
-		if (differencePrice.doubleValue() > quarter.doubleValue()){
-			differencePrice.subtract(quarter);
-			numQuarters = numQuarters +1;
+		while (difference.compareTo(BigDecimal.ZERO) > 0){
+		if (difference.doubleValue() >= quarter.doubleValue()){
+			difference = difference.subtract(quarter);
+			numQuarters = numQuarters.add(new BigDecimal(1));
 			
 			
-		} else if (differencePrice.doubleValue() > dime.doubleValue()){
-			differencePrice.subtract(dime);
-			numDimes = numDimes +1;
+		} else if (difference.doubleValue() >= dime.doubleValue()){
+			difference = difference.subtract(dime);
+			numDimes = numDimes.add(new BigDecimal(1));
 			
 		} else {
-			differencePrice.subtract(nickle);
-			numNickles = numNickles +1;
+			difference = difference.subtract(nickle);
+			numNickles = numNickles.add(new BigDecimal(1));
 			
 		}
 		}
 		
 		
-		totalChange = new BigDecimal(numQuarters * 0.25 + numDimes * 0.10 + numNickles * 0.05);
+		totalChange = BigDecimal.ZERO;
+		totalChange = totalChange.add(nickle.multiply(numNickles));
+		totalChange = totalChange.add(quarter.multiply(numQuarters));
+		totalChange = totalChange.add(dime.multiply(numDimes));
+		
+		
 		return "Your total change is: $" + totalChange.toString() + ": Quarters:" + numQuarters + ", Dimes:" + numDimes + ",  Nickes:" + numNickles;
 	}
 	
