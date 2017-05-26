3856655dfa60320ab6fcf9203e1e5ed44d70be3b
Connor McDevitt <connor1621@gmail.com>
2017-05-26 13:37:26 -0400

    Added missing tests for VendingMachineTest.java

--- module1-capstone/src/test/java/com/techelevator/VendingMachineTest.java
+++ module1-capstone/src/test/java/com/techelevator/VendingMachineTest.java
@@ -1,6 +1,10 @@
 package com.techelevator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +23,33 @@ public class VendingMachineTest {
 	public void tearDown() throws Exception {
 	}
 
+	
+	//vend() is inherently tested within each of the other methods' tests
+	//feedMoney() is inherently tested within each of the other methods' tests
+	@Test
+	public void testIsInStock() {
+		sut.refill();
+		assertTrue(sut.isInStock("A1"));
+		sut.feedMoney(new BigDecimal("20.00"));
+		for(int i = 0; i < 5; i++) {
+			sut.vend("A1");
+		}
+		assertFalse(sut.isInStock("A1"));
+	}
+	
+	@Test
+	public void testCanPurchase() {
+		sut.refill();
+		sut.feedMoney(new BigDecimal("6.00"));
+		assertTrue(sut.canPurchase("A1"));
+		sut.vend("A1");
+		assertFalse(sut.canPurchase("A1"));
+		sut.vend("D2");
+		sut.vend("C1");
+		sut.vend("D4"); //Should take us to $0.00
+		assertFalse(sut.canPurchase("D4"));	
+	}
+	
 	@Test
 	public void testDisplayItemsAfterRefill() {
 		String expectedOutput =
@@ -67,7 +98,6 @@ public class VendingMachineTest {
 			sut.vend("A2");
 		}
 		assertEquals(expectedOutput,sut.displayItems());
-		
 	}
 	
 	
